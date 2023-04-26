f1bb30c99417682d99dac0a3e5239bb111817658
namtruong <lactroimtp1v@gmail.com>
2023-04-26 17:11:31 +0700

    remove pulic/css folder

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -11,3 +11,4 @@ class SiteController {
 }
 
 module.exports = new SiteController();
+    
--- src/index.js
+++ src/index.js
@@ -5,9 +5,49 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+
+const {MongoClient} = require('mongodb');
+
+
+
+async function main() {
+	// we'll add code here soon
+
+    const uri = "mongodb+srv://dnt_store:dnt123456@cluster0.nf1pnoh.mongodb.net/test"
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+    
+        await listDatabases(client);
+     
+    } catch (e) {
+        console.error(e,'có lỗi');
+    }finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+main().catch(console.error);
+
+
+
+
+
+
 const route = require('./routes');
 
+
+// cấu hình đọc được các file tĩnh (hình ảnh, video, css ...) trong file public
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // nhận data từ method post
 app.use(
